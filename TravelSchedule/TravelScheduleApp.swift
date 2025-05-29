d8d3cba0fff4a48e0f13518c0dd4eed73645b4b2
ArtemSWeb <solodovnikov-artem@inbox.ru>
2025-05-29 22:30:44 +0300

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Артем Солодовников on 29.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
