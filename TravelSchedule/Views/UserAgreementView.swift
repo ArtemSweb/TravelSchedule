049bca7ea1c2234f9dda7ca7f57d13d0775109a1
ArtemSWeb <solodovnikov-artem@inbox.ru>
2025-07-17 00:42:31 +0300

    фикс цвета и отступов в вебвью пользовательского соглашения

--- TravelSchedule/Views/UserAgreementView.swift
+++ TravelSchedule/Views/UserAgreementView.swift
@@ -10,26 +10,35 @@ import WebKit
 
 struct UserAgreementView: View {
     @Environment(\.dismiss) var dismiss
-    let agreementURL = URL(string: "https://yandex.ru/legal/practicum_offer")!
     
     var body: some View {
-        WebView(url: agreementURL)
-            .navigationBarBackButtonHidden(true)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: { dismiss() }) {
-                        Image(.chevronLeft)
-                            .renderingMode(.template)
-                            .foregroundColor(.blackApp)
+        ZStack {
+            Color.whiteApp
+                .ignoresSafeArea()
+            
+            if let url = URL(string: "https://yandex.ru/legal/practicum_offer") {
+                WebView(url: url)
+                    .navigationBarBackButtonHidden(true)
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button(action: { dismiss() }) {
+                                Image(.chevronLeft)
+                                    .renderingMode(.template)
+                                    .foregroundColor(.blackApp)
+                            }
+                        }
+                        
+                        ToolbarItem(placement: .principal) {
+                            Text("Пользовательское соглашение")
+                                .font(.bold17)
+                                .foregroundColor(.blackApp)
+                        }
                     }
-                }
-                
-                ToolbarItem(placement: .principal) {
-                    Text("Пользовательское соглашение")
-                        .font(.bold17)
-                        .foregroundColor(.blackApp)
-                }
+            } else {
+                ServerErrorView()
             }
+        }
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
